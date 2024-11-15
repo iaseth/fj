533ea00424fa24f499b58023ca7ebb744e0c4492
Ankur Seth <iankurseth@gmail.com>
2024-11-15 22:47:03 +0530

    Now applying selected indent to output

--- src/fj.ts
+++ src/fj.ts
@@ -5,6 +5,9 @@ import { existsSync, readFileSync } from "fs";
 
 
 class Fjord {
+	tabs: boolean = false;
+	spaces: number = 4;
+
 	constructor () {}
 
 	format (inputJsonPath: string) {
@@ -12,11 +15,23 @@ class Fjord {
 		const inputJsonText = readFileSync(inputJsonPath, 'utf8');
 		try {
 			const jo = JSON.parse(inputJsonText);
-			console.log(jo);
+			// console.log(jo);
+			const outputJsonText = JSON.stringify(jo, null, this.getIndentation());
+			console.log(outputJsonText);
 		} catch (error) {
 			console.log(`\tError: ${error}`);
 			console.log(`\tCould not parse: '${inputJsonPath}'`);
 		}
+	}
+
+	getIndentation () {
+		return this.tabs ? '\t' : this.spaces;
+	}
+
+	useTabs () { this.tabs = true; }
+	useSpaces (spaces: number) {
+		this.tabs = false;
+		this.spaces = spaces;
 	}
 }
 
@@ -35,13 +50,13 @@ function doStuff (args: string[]) {
 				case 'MIN':
 					console.log(`\tLets minify this!`); break;
 				case 'S1': case '1S': case 'S':
-					console.log(`\tLets use 1 space!`); break;
+					console.log(`\tLets use 1 space!`); fj.useSpaces(1); break;
 				case 'S2': case '2S': case 'SS':
-					console.log(`\tLets use 2 spaces!`); break;
+					console.log(`\tLets use 2 spaces!`); fj.useSpaces(2); break;
 				case 'S4': case '4S': case 'SSSS':
-					console.log(`\tLets use 4 spaces!`); break;
+					console.log(`\tLets use 4 spaces!`); fj.useSpaces(4); break;
 				case 'TAB': case 'TABS': case 'T':
-					console.log(`\tLets use tabs!`); break;
+					console.log(`\tLets use tabs!`); fj.useTabs(); break;
 
 				case 'FJ':
 				case 'ASK':
